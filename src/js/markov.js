5cde277438dc8ef09e65cf1a525b27ca9f32b243
petejscott <pete.j.scott@gmail.com>
2014-12-23 10:30:54 -0500

    Change chain size number input to a range input

--- src/js/markov.js
+++ src/js/markov.js
@@ -2,14 +2,15 @@
 
 ; (function(win) {
 
-	var chainSize = 0;
-
 	var CONST_MK_INPUT = "#markovFile";
+	var CONST_MK_CHAINSIZE = "#markovChainSize";
+	var CONST_MK_SELECTED_CHAINSIZE = "#selectedChainSize";
 	var CONST_MK_LOAD = "#markovLoad";
 	var CONST_MK_SUBMIT = "#markovSubmit";
 	var CONST_MK_OUTPUT = "#markovOutput";
 
 	var sourceText = null;
+	var chainSize = 0;
 	var dict = null;
 	var wordSet = null;
 
@@ -49,17 +50,41 @@
 		};
 		reader.readAsText(file);
 	} 
+	
+	function setChainSize(e) {	
+		wordSet = null;
+		dict = null;
+		disableStep(3);
+		var chainSizeElement = win.document.querySelector(CONST_MK_CHAINSIZE);
+		chainSize = chainSizeElement.value;		
+		var chainSizeDescription = "";
+		switch (chainSize) {
+			case "1":
+				chainSizeDescription = "rather nonsensical";
+				break;
+			case "2":
+				chainSizeDescription = "somewhat nonsensical";
+				break;
+			case "3":
+				chainSizeDescription = "somewhat sensible";
+				break;
+			case "4":
+				chainSizeDescription = "mostly sensible";
+				break;
+		}
+		var selectedChainSizeElement = win.document.querySelector(CONST_MK_SELECTED_CHAINSIZE);
+		selectedChainSizeElement.textContent = "(" + chainSizeDescription + ")";
+	}
 
 	function setWords(source) {
 		wordSet = null;
 		dict = null;
+		setChainSize(null);
 		var dictStatus = win.document.querySelector(".dictStatus");
 		dictStatus.textContent = "Building...";
 		var text = source.replace(/[\r\n]/gi, ' ');
 		var el = win.document.querySelector("#source");
 		wordSet = markovWordsetBuilder.addWords(text, ' ');
-		var chainSizeElement = win.document.querySelector("#markovChainSize");
-		chainSize = chainSizeElement.value;
 		var dictionaryWorker = new Worker("js/markovDictionaryWorker.js");
 		dictionaryWorker.onmessage = function(e) {
 			dict = e.data.dict;
@@ -85,6 +110,10 @@
 		var wordFile = win.document.querySelector(CONST_MK_INPUT);
 		wordFile.addEventListener("change", function(e) { readFile(e); });
 
+		// bind change event to chain size selection
+		var wordFile = win.document.querySelector(CONST_MK_CHAINSIZE);
+		wordFile.addEventListener("change", function(e) { setChainSize(e); });
+		
 		// bind click event to build dictionary button
 		var wordFile = win.document.querySelector(CONST_MK_LOAD);
 		wordFile.addEventListener("click", function(e) { setWords(sourceText); });
@@ -98,6 +127,7 @@
 		disableStep(2);
 		disableStep(3);
 		readFile(null); // detect any file already set even if the change event hasn't fired
+		setChainSize();
 	}
 
 	bind();
