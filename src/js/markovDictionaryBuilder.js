2d465a7009af4ab879d042f6c5781b4e5d0bf97f
petejscott <pete.j.scott@gmail.com>
2014-12-24 10:51:01 -0500

    Store the dictItem keys in a map to avoid searching the dict for objects with matching keys (this cut performance tests from 5000ms to 150ms)

--- src/js/markovDictionaryBuilder.js
+++ src/js/markovDictionaryBuilder.js
@@ -3,15 +3,17 @@
 var markovDictionaryBuilder = (function() {
 
 	function buildDict(wordSet, chainSize) {
+
 		console.log("building dictionary from " + wordSet.length + " words with a chain size of " + chainSize);
 
+		var map = [];
 		var dict = [];
 		for (var i = 0, len = wordSet.length - chainSize; i < len; i++) {
 			
 			var end = i + parseFloat(chainSize); 
 			var workingSet = wordSet.slice(i, end+1);
 			var k = workingSet.slice(0, workingSet.length-1);
-			var n = [workingSet.slice(-1)];
+			var n = workingSet.slice(-1);
 			
 			var dictItem = {
 				'key' : k.join('/').toLowerCase(),
@@ -19,26 +21,19 @@ var markovDictionaryBuilder = (function() {
 				'next' : n
 			};
 			
-			var match = getDictItemByKey(dict, dictItem.key);
-		  
-			if (match !== null) {
-				var index = dict.indexOf(match);
-				dict[index].next.push(dictItem.next[0]);
-			} else {
+			var mi = map[dictItem.key];
+			if (typeof(mi) === 'undefined') {
+				map[dictItem.key] = dict.length;
 				dict.push(dictItem);
+			} else {
+				dict[mi].next.push(dictItem.next[0]);
 			}
 		}
-		//console.log(dict);
 		return dict;
 	}
 
-	function getDictItemByKey(dict, key) {
-		for (var i = 0, len = dict.length; i < len; i++) {
-			if (dict[i].key === key) return dict[i];
-		}
-		return null;
-	}
-
-	return { buildDict : buildDict  };
+	return { 
+		buildDict : buildDict
+	};
 
 })();
