5ae88fb88fcf77cf8232e3f89d9266ec34bc200d
petejscott <pete.j.scott@gmail.com>
2014-12-24 10:17:45 -0500

    Shave a couple hundred milliseconds off the dictionary builder

--- src/js/markovDictionaryBuilder.js
+++ src/js/markovDictionaryBuilder.js
@@ -1,16 +1,17 @@
 'use strict';
 
 var markovDictionaryBuilder = (function() {
-  
+
 	function buildDict(wordSet, chainSize) {
 		console.log("building dictionary from " + wordSet.length + " words with a chain size of " + chainSize);
 
 		var dict = [];
 		for (var i = 0, len = wordSet.length - chainSize; i < len; i++) {
 			
-			var end = i + parseFloat(chainSize);
-			var k = wordSet.slice(i, end);
-			var n = [wordSet[end]];
+			var end = i + parseFloat(chainSize); 
+			var workingSet = wordSet.slice(i, end+1);
+			var k = workingSet.slice(0, workingSet.length-1);
+			var n = [workingSet.slice(-1)];
 			
 			var dictItem = {
 				'key' : k.join('/').toLowerCase(),
